efaa9af855844c14870613e80ac17e6a24f2500f
Jonny Cross <crossjonny5@gmail.com>
2020-08-13 21:01:54 -0700

    Added title to sign in page

--- taskManager/SignIn.Designer.cs
+++ taskManager/SignIn.Designer.cs
@@ -29,20 +29,35 @@
         private void InitializeComponent()
         {
             this.nameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // nameBox
             // 
-            this.nameBox.Location = new System.Drawing.Point(225, 82);
+            this.nameBox.Location = new System.Drawing.Point(188, 223);
             this.nameBox.Name = "nameBox";
             this.nameBox.Size = new System.Drawing.Size(100, 26);
             this.nameBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(178, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sign In to TMS";
             // 
             // SignIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.nameBox);
             this.Name = "SignIn";
             this.Text = "SignIn";
@@ -54,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label1;
     }
 }
